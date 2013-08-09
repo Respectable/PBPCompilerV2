cd97b013b6a79645158fe19c47eae59d6da8670b
Respectable <aodell91@gmail.com>
2013-08-08 20:34:35 -0500

    Finished init write up of compileVerifier

--- Compiler/src/compileVerification/CompileVerifier.java
+++ Compiler/src/compileVerification/CompileVerifier.java
@@ -21,12 +21,13 @@ public class CompileVerifier
 			Connection conn = DriverManager.getConnection(path,userName,password);
 			CallableStatement cStmt = conn.prepareCall("{call box_score(?)}");
 			cStmt.setString(1, stats.get(0).getGameID());
+			ResultSet rs;
 			
 			boolean hadResults = cStmt.execute();
 			
 			while(hadResults)
 			{
-				ResultSet rs = cStmt.getResultSet();
+				rs = cStmt.getResultSet();
 				
 				while(rs.next())
 			    {
@@ -35,6 +36,10 @@ public class CompileVerifier
 					if (currentPlayer == null)
 					{
 						return false;
+					}
+					if (!currentPlayer.getComment().equals(""))
+					{
+						continue;
 					}
 					if ((rs.getInt("FGM") != currentPlayer.getFgm())
 						|| (rs.getInt("FGA") != currentPlayer.getFga())
@@ -49,12 +54,41 @@ public class CompileVerifier
 						|| (rs.getInt("PF") != currentPlayer.getPf())
 						|| (rs.getInt("PTS") != currentPlayer.getPts()))
 					{
+						System.out.println(currentPlayer.getGameID() +
+								" Error on player " + currentPlayer.getPlayerName());
+						if (rs.getInt("FGM") != currentPlayer.getFgm())
+							System.out.println("Field Goals Made is incorrect");
+						if (rs.getInt("FGA") != currentPlayer.getFgm())
+							System.out.println("Field Goals Attempted is incorrect");
+						if (rs.getInt("FTM") != currentPlayer.getFgm())
+							System.out.println("Free Throws Made is incorrect");
+						if (rs.getInt("FTA") != currentPlayer.getFgm())
+							System.out.println("Free Throws Attempted is incorrect");
+						if (rs.getInt("OREB") != currentPlayer.getFgm())
+							System.out.println("Off. Rebound is incorrect");
+						if (rs.getInt("DREB") != currentPlayer.getFgm())
+							System.out.println("Def. Rebound  is incorrect");
+						if (rs.getInt("AST") != currentPlayer.getFgm())
+							System.out.println("Assists  are incorrect");
+						if (rs.getInt("TO") != currentPlayer.getFgm())
+							System.out.println("Turnovers  are incorrect");
+						if (rs.getInt("STL") != currentPlayer.getFgm())
+							System.out.println("Steals  are incorrect");
+						if (rs.getInt("BLK") != currentPlayer.getFgm())
+							System.out.println("Blocks  are incorrect");
+						if (rs.getInt("PF") != currentPlayer.getFgm())
+							System.out.println("Personal Fouls are incorrect");
+						if (rs.getInt("PTS") != currentPlayer.getFgm())
+							System.out.println("Points are incorrect");
 						return false;
 					}
 			    }
 				
 				hadResults = cStmt.getMoreResults();
 			}
+			cStmt.close();
+			conn.close();
+			
 		} 
 		catch (SQLException e) 
 		{
@@ -68,9 +102,26 @@ public class CompileVerifier
 		return true;
 	}
 	
-	public static void delete(String gameID)
+	public static void delete(String gameID, String path,
+			String userName, String password)
 	{
-		//TODO
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(path,userName,password);
+			CallableStatement cStmt = conn.prepareCall("{call delete_game(?)}");
+			cStmt.setString(1, gameID);
+			cStmt.executeUpdate();
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
 	}
 	
 	private static PlayerStatsJson findPlayer(int ID, 
